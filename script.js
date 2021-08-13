be4312923aa8cb454b9612d93b34a7e1b59ede37
asil <asdewkjajc@mail.ru>
2021-08-13 13:26:33 +0500

    java2

---
+++ script.js
@@ -0,0 +1,16 @@
+var name = prompt('Введите имя'),
+    surname = prompt('Введите фамилию'),
+    year = +prompt('Введите год рождения');
+console.log(`Мое имя ${name}. Моя фомилия ${surname}. Мой год рождения ${year}`);
+ 
+var num1 = +prompt('Решите примет 6+3=?');
+console.log(`6+3=9. Ваш ответ; ${num1}`);
+var num2 = +prompt('Решите примет 20*5=?');
+console.log(`20*5=100. Ваш ответ; ${num2}`);
+var num3 = +prompt('Решите примет 15/5=?');
+console.log(`15/5=3. Ваш ответ; ${num3}`);
+var num4 = +prompt('Решите примет 90-27=?');
+console.log(`90-27=73. Ваш ответ; ${num4}`);
+
+var abs = ('ТЕПЕРЬ ЗАЙДИТЕ В КОНСОЛЬ!!!')
+document.write(abs)
