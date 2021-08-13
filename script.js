77322869e95cc3045beca4883682818a7d8b8fef
asil <asdewkjajc@mail.ru>
2021-08-13 13:42:26 +0500

    java2

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ var name = prompt('Введите имя'),
     year = +prompt('Введите год рождения');
 console.log(`Мое имя ${name}. Моя фомилия ${surname}. Мой год рождения ${year}`);
  
-var num1 = +prompt('Решите примет 6+3=?');
+var num1 = +prompt('Решите пример 6+3=?');
 console.log(`6+3=9. Ваш ответ; ${num1}`);
-var num2 = +prompt('Решите примет 20*5=?');
+var num2 = +prompt('Решите пример 20*5=?');
 console.log(`20*5=100. Ваш ответ; ${num2}`);
-var num3 = +prompt('Решите примет 15/5=?');
+var num3 = +prompt('Решите пример 15/5=?');
 console.log(`15/5=3. Ваш ответ; ${num3}`);
-var num4 = +prompt('Решите примет 90-27=?');
+var num4 = +prompt('Решите пример 90-27=?');
 console.log(`90-27=73. Ваш ответ; ${num4}`);
 
 var abs = ('ТЕПЕРЬ ЗАЙДИТЕ В КОНСОЛЬ!!!')
